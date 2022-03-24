09a5b4421c358f3215363d05ff36e3b33eea6300
kumarb97 <kumarbamankar@gmail.com>
2022-03-24 13:01:11 +0530

    Step1:Ability to add calculate fare

--- src/main/java/cabinvoiceMain/CabInvoice.java
+++ src/main/java/cabinvoiceMain/CabInvoice.java
@@ -1,10 +1,19 @@
+/*
+ * Step-1_Given distance and time , invoice generator should return total fare.
+ */
 package cabinvoiceMain;
 
 public class CabInvoice {
 	public double COST_PER_KM = 10;
 	public int COST_PER_TIME = 1;
 	public int MINIMUM_FARE = 5;
-
+	
+	/**
+	 * Method calculateFare  is used to calculate fare of cab service.
+	 * @param distance
+	 * @param time
+	 * @return fare
+	 */
 	public double calculateFare(double distance,int time) {
 		double fare = (COST_PER_KM * distance)+(COST_PER_TIME*time);
 		return fare;
--- src/test/java/cabinvoiceTest/cabInvoiceTest.java
+++ src/test/java/cabinvoiceTest/cabInvoiceTest.java
@@ -2,17 +2,18 @@ package cabinvoiceTest;
 
 import org.junit.Assert;
 import org.junit.Test;
-
 import cabinvoiceMain.CabInvoice;
+
 public class cabInvoiceTest {
 	
+	/*
+	 * @Purpose : Testing code total fare of cab service we are getting or not.
+	 */
 	@Test
 	public void givenDitanceAndTime_shouldReturnTotalFare(){
 		CabInvoice cabinvoice = new CabInvoice();
 		double result = cabinvoice.calculateFare(2.0,5);
-		Assert.assertEquals(25,result,0.0);
-		
-		
+		Assert.assertEquals(25,result,0.0);		
 	}
 	
 
